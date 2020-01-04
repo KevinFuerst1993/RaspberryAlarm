bf2d44d5a79c93211f8e0d9caf94227afb1d9e2b
Kevin Fuerst <kevin.fuerst@gawnet.ch>
2020-01-04 18:40:11 +0100

    forgot to add in last commit

--- php/alarm.php
+++ php/alarm.php
@@ -3,6 +3,12 @@
 error_reporting(-1);
 ini_set('display_errors','On');
 
+$descriptorspec = array(
+   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+   2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
+);
+
 //open log file
 $logFile = fopen("/var/www/html/phpLog","a");
 $timeStamp = date("Y-m-d H:i:s");
@@ -23,13 +29,15 @@ if ($row["id"])
 {
 	fwrite($logFile, "\talarm time reached\n");
 	fclose($logFile);
-	system('/usr/bin/nohup /usr/bin/ogg123 '.$home."sound/".$row["sound"].'>/dev/null  &' );
-	system('/usr/bin/nohup '.$home."light/".$row["light"].'>/dev/null  &' );
-	exec('/usr/local/bin/gpio mode 0 in');
+	//system('/usr/bin/nohup /usr/bin/ogg123 '.$home."sound/".$row["sound"].'>/dev/null  &' );
+	//system('/usr/bin/nohup '.$home."light/".$row["light"].'>/dev/null  &' );
+	//$handle = proc_open("/home/pi/Repos/RaspberryAlarm/light/".$row["light"].'>/dev/null  &' , $descriptorspec, $pipes);
+	$handle = proc_open("/home/pi/Repos/RaspberryAlarm/light/sunrise", $descriptorspec, $pipes);
+	exec('/usr/bin/gpio mode 0 in');
 	$sql="delete from alarm where id=".$row["id"];
 	$con -> query($sql);
 	$sts=time();
-	while(!system('/usr/local/bin/gpio read 0',$o)) 
+	while(system('/usr/bin/gpio read 0',$o)) 
 	{
 		if(time()> ($sts+60)) 
 		{
@@ -38,6 +46,24 @@ if ($row["id"])
 			break;
 		}
 	}
+	if(system('/usr/bin/gpio read 0',$o))
+	{
+		fwrite($logFile, "button pressed\n");
+	}
+	
+	//stop led proccess
+	$status = proc_get_status($handle);
+	$ppid = $status['pid'];
+	//use ps to get all the children of this process, and kill them
+	$pids = preg_split('/\s+/', `ps -o pid --no-heading --ppid $ppid`);
+	foreach($pids as $pid) {
+	  if(is_numeric($pid)) {
+		  echo "Killing $pid\n";
+		  posix_kill($pid, 9); //9 is the SIGKILL signal
+	  }
+	}
+	proc_close($handle);
+	
 	system($home."light/all_off" );
 }
 system('/usr/bin/killall ogg123 ');
